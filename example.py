2256376f84ae19d84f0d35d71f3f6e1a0d77b88a
Mark Blumenau <71517531+burunduk387@users.noreply.github.com>
2021-10-18 19:01:59 +0300

    First commit

---
+++ example.py
@@ -0,0 +1,10 @@
+ser = serial.Serial(
+    port="COM3",
+    baudrate=9600,
+    timeout=0.5
+)
+ser.close()
+
+ser.open()
+ser.write(ask_value_cmd(16, 1))
+print(float(read_mv()))
---
+++ func.py
@@ -0,0 +1,21 @@
+import serial
+def calc_chk(s):
+    sum = 0
+    for c in s:
+        sum += ord(c)
+    sum = sum % 256
+    return '%2X' % (sum & 0xFF)
+def ask_value_cmd(device_address, channel):
+    device_address = hex(device_address)[2:]
+    channel = str(channel)
+    cmd = "#" + device_address + channel
+    cmd = cmd + calc_chk(cmd) + "\r"
+    return cmd.encode("utf-8")
+def read_mv():
+    ans = ""
+    for i in range(32):
+        ch = ser.read().decode("ascii")
+        ans += ch
+        if ch == "\r":
+            break
+    return ans[1:-3]
